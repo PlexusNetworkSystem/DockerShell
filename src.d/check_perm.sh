ce473adbcc0630252860cfdd07e3dd193e9c7275
Ege KILIÇ <aegean.sword1@gmail.com>
2023-12-04 22:04:00 +0300

    New version

--- src.d/check_perm.sh
+++ src.d/check_perm.sh
@@ -2,10 +2,12 @@ file_path="/var/run/docker.sock"
 
 while [ "$(stat -c "%a" "$file_path")" != "777" ]; do
         # Set the permissions using sudo
-        if [[ -f /usr/bin/zenity ]]; then
-            sudo_passwd=$(zenity --entry --title="Sudo Password" --text="Type sudo password for access to Docker:")
+        if [[ -f /usr/bin/pkexec ]]; then
+            pkexec chmod 777 "$file_path"
+
         else
             read -e -p "$(echo -ne "Type sudo password for get access to docker: ")" sudo_passwd
+            echo $sudo_passwd | sudo -S chmod 777 "$file_path"
         fi
-        echo $sudo_passwd | sudo -S chmod 777 "$file_path"
+        
 done
